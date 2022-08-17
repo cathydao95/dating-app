0442331302156642adffed01cf15f53a9790f375
Cathy Dao <cathydao95@gmail.com>
2022-08-16 22:11:21 -0700

    add dashboard for chat matches

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,68 @@
+import TinderCard from "react-tinder-card";
+import { useState } from "react";
+
+import ChatContainer from "../components/ChatContainer";
 function Dashboard() {
-  return <div></div>;
+  const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://imgur.com/Q9WPlWA.jpg",
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://imgur.com/MWAcQRM.jpg",
+    },
+    {
+      name: "Monica Hall",
+      url: "https://imgur.com/OckVkRo.jpg",
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://imgur.com/dmwjVjG.jpg",
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://imgur.com/Gg6BpGn.jpg",
+    },
+  ];
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+
+  return (
+    <div className="dashboard">
+      <ChatContainer />
+      <div className="swipe-container">
+        <div className="card-container">
+          {characters.map((character) => (
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+        </div>
+        <div className="swipe-info">
+          {lastDirection ? <p>You swiped {lastDirection} </p> : <p></p>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
