a3618bd7be3bf9f664f94bb0840f644676792d89
Cathy Dao <cathydao95@gmail.com>
2022-08-16 22:11:26 -0700

    edits to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,16 +22,12 @@ function Home() {
         setIsSignUp={setIsSignUp}
       />
       <div className="home">
-        <h1>Swipe Right</h1>
+        <h1 className="primary-title">Swipe Right</h1>
         <button className="primary-button" onClick={handleClick}>
           {authToken ? "Log Off" : "Create Account"}
         </button>
         {showModal && (
-          <AuthModal
-            setShowModal={setShowModal}
-            isSignUp={isSignUp}
-            setIsSignUp={setIsSignUp}
-          />
+          <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
         )}
       </div>
     </div>
